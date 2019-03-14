dde56bed356e01cb0ebc780d28960cc0374444ca
Chaz <ctol1994@gmail.com>
2019-03-13 19:36:01 -0700

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var firebase = require("firebase");
 
+var config = {
+  apiKey: "AIzaSyB_BOnrLIbOsgFf2U84pOXYJagwGPltLaM",
+  authDomain: "cziscavengerhunt.firebaseapp.com",
+  databaseURL: "https://cziscavengerhunt.firebaseio.com",
+  projectId: "cziscavengerhunt",
+  storageBucket: "cziscavengerhunt.appspot.com",
+  messagingSenderId: "812384034022"
+};
+firebase.initializeApp(config);
+
+const functions = require('firebase-functions');
+
+var db = firebase.firestore();
+  
 class App extends Component {
+
   render() {
+    var citiesRef = db.collection('users').doc('userTypes').collection('students');
+var allCities = () => {citiesRef.get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+}
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +38,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <script>{allCities()}</script>
           <a
             className="App-link"
             href="https://reactjs.org"
