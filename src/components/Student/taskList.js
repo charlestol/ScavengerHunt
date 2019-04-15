c1cbb2d6175d01834adcab618e0e025074549cb1
Chaz <ctol1994@gmail.com>
2019-04-15 10:35:58 -0700

    completed tasks list

--- src/components/Student/taskList.js
+++ src/components/Student/taskList.js
@@ -9,7 +9,8 @@ class EventList extends Component {
 
     this.state = {
       loading: false,
-      tasks: []
+      tasks: [],
+      completed: [],
     };
   }
 
@@ -17,31 +18,52 @@ class EventList extends Component {
     this.setState({ loading: true });
     let ac = this.props.match.params.eventId;
 
+    let email = this.props.email;
+
     this.props.firebase.scavengerHuntTasks(ac)
     .onSnapshot(querySnapshot => {
         let tasks = [];
+        let completed = [];
         querySnapshot.forEach(doc => {
             let data = doc.data();
-            tasks.push(data);
+            let task = doc.data().name;
+            console.log(data)
+
+           this.props.firebase.scavengerHuntSubmission(ac, email, task).get()
+           .then(doc => {
+             if(doc.exists) {
+               completed.push(data)
+             } else {
+               tasks.push(data)
+             }
+           })
         });
+
 
         this.setState({
           tasks,
+          completed,
           loading: false,
         });
       });
   }
 
   render() {
-    const { tasks, loading } = this.state;
+    const { tasks, completed, loading } = this.state;
     // console.log('list', tasks)
     const URL = this.props.match.url;
 
     return (
       <div>
-        <h2>Tasks</h2>
         {loading && <div>Loading ...</div>}
+        <h2>On-Going Tasks</h2>
         {tasks.map(task => (
+            <div key={task.name}>
+                <Link to={`${URL}/${task.name}`}>{task.name}</Link>
+            </div>
+        ))}
+         <h2>Completed Tasks</h2>
+        {completed.map(task => (
             <div key={task.name}>
                 <Link to={`${URL}/${task.name}`}>{task.name}</Link>
             </div>
