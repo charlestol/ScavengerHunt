e1911a52dda6b6649d0a7048c198ddf78121867e
Chaz <ctol1994@gmail.com>
2019-04-16 17:35:31 -0700

    handle wrong start and end dates

--- src/components/Instructor/eventCreate.js
+++ src/components/Instructor/eventCreate.js
@@ -14,8 +14,12 @@ const INITIAL_STATE = {
     dateEnd: null,
     description: '',
     courses: '',
-    error: null
+    error: null,
+    dateError: null
 }
+
+const ERROR_START_DATE = "Your start date can't be greater than your end date, please confirm that your start and end dates are correct.";
+const ERROR_END_DATE = "Your end date can't be less than your start date, please confirm that your start and end dates are correct.";
 
 class CreateEvent extends Component {
     constructor(props) {
@@ -65,12 +69,34 @@ class CreateEvent extends Component {
     this.setState({ [event.target.name]: event.target.value });
     };
 
-    onStartDateSelect = (date) => {
-        this.setState({ dateStart: date})
+    handleChangeStart = (date) => {
+        let { dateEnd } = this.state; 
+        if(dateEnd !== null && dateEnd < date) {
+            this.setState({ 
+                dateStart: dateEnd,
+                dateError: ERROR_START_DATE
+            });
+        } else {
+            this.setState({ 
+                dateStart: date,
+                dateError: null 
+            })
+        }
     }
 
-    onEndDateSelect = (date) => {
-        this.setState({ dateEnd: date})
+    handleChangeEnd = (date) => {
+        let { dateStart } = this.state; 
+        if(dateStart !== null && dateStart > date) {
+            this.setState({ 
+                dateEnd: dateStart,
+                dateError: ERROR_END_DATE
+            });
+        } else {
+            this.setState({ 
+                dateEnd: date,
+                dateError: null
+            });
+        }
     }  
 
     render() {
@@ -81,7 +107,8 @@ class CreateEvent extends Component {
             dateEnd,
             description,
             courses,
-            error
+            error,
+            dateError
         } = this.state;
 
         const isInvalid = 
@@ -90,7 +117,10 @@ class CreateEvent extends Component {
             description === '' ||
             courses === '' ||
             dateStart === null ||
-            dateEnd === null;
+            dateEnd === null ||
+            dateError !== null;
+
+        console.log(dateStart)
 
         return (
             <AuthUserContext.Consumer>
@@ -115,24 +145,32 @@ class CreateEvent extends Component {
                             <br />
                             <DatePicker
                                 selected={dateStart}
-                                onChange={this.onStartDateSelect}
+                                onChange={this.handleChangeStart}
+                                selectsStart
+                                startDate={dateStart}
+                                endDate={dateEnd}
                                 showTimeSelect
                                 timeFormat="HH:mm"
                                 timeIntervals={60}
                                 dateFormat="MMMM d, yyyy h:mm aa"
                                 timeCaption="Time"
                                 placeholderText="Click to set Start time"
+                                shouldCloseOnSelect={false}
                             />
                             <br />
                             <DatePicker
                                 selected={dateEnd}
-                                onChange={this.onEndDateSelect}
+                                onChange={this.handleChangeEnd}
+                                selectsEnd
+                                startDate={dateStart}
+                                endDate={dateEnd}
                                 showTimeSelect
                                 timeFormat="HH:mm"
                                 timeIntervals={60}
                                 dateFormat="MMMM d, yyyy h:mm aa"
                                 timeCaption="Time"
                                 placeholderText="Click to set End time"
+                                shouldCloseOnSelect={false}
                             />
                             <br />
                             <input
@@ -155,6 +193,7 @@ class CreateEvent extends Component {
                                 Create
                             </button>
                             <br />
+                            {dateError && <p>{dateError}</p>}
                             {error && <p>{error}</p>}
                         </form>
                     </div>
