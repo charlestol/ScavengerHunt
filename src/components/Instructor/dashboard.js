b22b400f118c24aa1663b85360b81e3a25bbbe97
Chaz <ctol1994@gmail.com>
2019-05-12 14:36:29 -0700

    tabs for instructor dash

--- src/components/Instructor/dashboard.js
+++ src/components/Instructor/dashboard.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { AuthUserContext, withAuthorization } from '../Session';
 import * as ROLES from '../../constants/roles';
 import CreateEvent from './eventCreate';
 import EventHistory from './eventHistory';
 import ActiveEvents from './eventsActive';
+import { Container, Col, Row, Button } from 'reactstrap'
 
-const AdminPage = () => (
-  <AuthUserContext.Consumer>
-    {authUser => (
-      <div className="dashboard my-4">
-        <CreateEvent />
-        <ActiveEvents user={authUser} />
-        <EventHistory user={authUser} />
-      </div>
-    )}
-  </AuthUserContext.Consumer>
-);
+const layout = {
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
+
+class AdminPage extends Component {
+  state = {
+    activeTab: 1
+  }
+  
+  toggleActive = () => {
+    if (this.state.activeTab !== 1) {
+      this.setState({
+        activeTab: 1
+      });
+    }
+  }
+
+  toggleCreate = () => {
+    if (this.state.activeTab !== 2) {
+      this.setState({
+        activeTab: 2
+      });
+    }
+  }
+
+  toggleHistory = () => {
+    if (this.state.activeTab !== 3) {
+      this.setState({
+        activeTab: 3
+      });
+    }
+  }
+
+  render() {
+    const { activeTab } = this.state;
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <Container>
+            <Col md="12" lg={{ size: 6}} style={layout}>
+               <Row style={layout}>
+                 <Button outline color="danger" onClick={this.toggleActive} active={activeTab===1} className="mb-3 mx-1 rounded-sm">Active Events</Button>
+                 <Button outline color="danger" onClick={this.toggleCreate} active={activeTab===2} className="mb-3 mx-1 rounded-sm">Create Event</Button>
+                 <Button outline color="danger" onClick={this.toggleHistory} active={activeTab===3} className="mb-3 mx-1 rounded-sm">Event History</Button>
+               </Row>
+               {activeTab===1 && <ActiveEvents user={authUser} />}
+               {activeTab===2 && <CreateEvent />}
+               {activeTab===3 && <EventHistory user={authUser} />}
+            </Col>
+          </Container>
+        )}
+      </AuthUserContext.Consumer>
+    );
+  }
+}
 
 const condition = authUser =>
   authUser && authUser.role===ROLES.INSTRUCTOR;
